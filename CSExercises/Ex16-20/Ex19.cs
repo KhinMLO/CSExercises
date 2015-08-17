25c8bf084c08144eb356cea29e25bbaf0f473540
KhinMLO <khinmarlwinoo27@gmail.com>
2015-08-17 17:23:19 +0800

    FinishedEx21-26

--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -6,7 +6,26 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-
+            double cost = 2.4, dist;
+            Console.Write("Enter distance by km : ");
+            bool res = double.TryParse(Console.ReadLine(), out dist);
+            double dist1 = Math.Ceiling(dist * 10);
+            if(res == true)
+            {
+                if (dist1 > 90)
+                {
+                    cost = cost + (85 * 0.04) + ((dist1 - 90) * 0.05);
+                    Console.WriteLine("Total cost of the distance {0:C}" , cost);
+                }
+                else if (dist1 > 0.5 && dist1 < 90)
+                {
+                    cost = cost + (85 * 0.04);
+                    Console.WriteLine("Total cost of the distance {0:C}" , cost);
+                }
+                else
+                    Console.WriteLine("Total cost of the distance {0:C}" , cost);
+            }
+            Console.WriteLine("Please enter only double value");
         }
     }
 }
--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -1,9 +1,34 @@
-﻿namespace CSExercises
+﻿using System;
+
+namespace CSExercises
 {
     public class Ex20
     {
         public static void Main(string[] args)
         {
+            double tv, dvd, mp3;
+            double cost, dis;
+            Console.Write("Enter quantity for TV : ");
+            tv = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter quantity for DVD : ");
+            dvd = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter quantity for MP3 : ");
+            mp3 = Convert.ToDouble(Console.ReadLine());
+            cost = tv + dvd + mp3;
+            if (cost > 5000 && cost <= 10000)
+            {
+                dis = (tv + dvd) * 0.1;
+                dis = cost - dis;
+                Console.WriteLine("Total price for this order is {0:C}", dis);
+            }
+            else if (cost > 10000)
+            {
+                dis = (tv + dvd) * 0.15;
+                dis = cost - dis;
+                Console.WriteLine("Total price for this order is {0:C}", dis);
+            }
+            else
+                Console.WriteLine("Total price for this order is {0:C}", cost);
             
         }
     }
